851c5450c2b3b5284532f7e01ad6ff6486568180
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-08 17:36:44 -0600

    Added comments for where each key aspect of the project should go.

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -1,1 +1,17 @@
-# Test Push and Commit
+# Title
+
+# Main While Loop
+
+    # Get Stock Option - Incomplete
+    
+    # Select Chart Type - Incomplete
+
+    # Select Time Series Type - Incomplete
+
+    # Select Start Date - Incomplete
+
+    # Select End Date - Incomplete
+
+    # Generate Graph and Open in Users Default Browser - Incomplete
+
+    # Find another stock - Inomplete
