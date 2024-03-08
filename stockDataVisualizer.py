d914a05d9b21733fdfc31ff17d23ce3428afcd62
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-08 17:45:49 -0600

    Updated stockDataVisualizer.py
    Added the main while loop and the ability to break out of it.

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -3,7 +3,8 @@ print("Stock Data Visualizer")
 print("---------------------")
 
 # Main While Loop
-
+loop = True
+while (loop == True):
     # Get Stock Option - Incomplete
     
     # Select Chart Type - Incomplete
@@ -16,4 +17,11 @@ print("---------------------")
 
     # Generate Graph and Open in Users Default Browser - Incomplete
 
-    # Find another stock - Inomplete
+    # Find another stock - Complete
+    while(True):
+        visualizeAnother = input("Would you like to view more stock data? Press 'y' to continue : ")
+        if (visualizeAnother == 'y'):
+            break
+        else:
+            loop = False
+            break
