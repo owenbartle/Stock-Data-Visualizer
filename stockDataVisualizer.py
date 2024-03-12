823c4de24feab05730bbc165755b1108f7e7fac6
seanmoserr <seanmoserr@gmail.com>
2024-03-12 12:12:59 -0500

    Program now takes user time series option, user symbol, and our api key and concatenates them into a url variable that will be used to query the API

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -7,7 +7,7 @@ import requests
 api_key = 'JLFXYX4J4I20CF8E'
 
 #method for fetching stock date - In Progress
-def fetch_stock_date(symbol, api_key):
+def fetch_stock_date(symbol, api_key, url):
     #url
     #response
     #data
@@ -15,7 +15,7 @@ def fetch_stock_date(symbol, api_key):
 
 # Title
 print("Stock Data Visualizer")
-print("---------------------")
+print("-------------------------")
 
 # Main While Loop
 loop = True
@@ -33,13 +33,14 @@ while (loop == True):
         else:
             print("Must choose a valid type (1. Bar Graph | 2. Line Graph)")
 
-    # Select Time Series Type - Incomplete
+    # Select Time Series Type - Complete
     print("Select the Time Series of the chart you want to Generate:")
     print("----------------------------")
     print("1. Intraday")
     print("2. Daily")
     print("3. Weekly")
     print("4. Monthly\n")
+    
     bool = True
     while(bool):
         tsChoice = input("Enter time series option (1, 2, 3, 4): ")
@@ -51,12 +52,25 @@ while (loop == True):
                 raise Exception("Not a valid choice. Enter only a '1', '2', '3', or '4', Exception")
         except:
             print("Not a valid choice. Enter only a '1', '2', '3', or '4'.", Exception)
+    
+    tsString = ' '
+        
+    ##Convert user time series type to api url parameter
+    if(tsChoice == 1):
+        tsString = "function=TIME_SERIES_INTRADAY"
+    elif(tsChoice == 2):
+        tsString = "function=TIME_SERIES_DAILY"
+    elif(tsChoice == 3):
+        tsString = "function=TIME_SERIES_WEEKLY"
+    else:
+        tsString = "function=TIME_SERIES_MONTHLY"
+        
        
         
     # Select Start Date - Complete
     # Select End Date - Complete
     while (True):
-        startDate = input("Enter the desired Start Date (YYYY-MM-DD): ")
+        startDate = input("\nEnter the desired Start Date (YYYY-MM-DD): ")
         endDate = input("Enter the desired End Date (YYYY-MM-DD): ")
         try:
             parsedStartDate = datetime.strptime(startDate, "%Y-%m-%d")
@@ -67,6 +81,19 @@ while (loop == True):
                 print("The end date must be after the start date.")
         except ValueError:
             print("Must input a valid date format. Ex: (YYYY-MM-DD)")
+            
+    
+    
+    ##change user choices to api parameters
+    ##variables tsString for time series option,
+    ##symbol={option}
+    
+    ##url now queries our api using our key, searching for ouyr given symbol, and using our time series option
+    
+    url = f"https://www.alphavantage.co/query?{tsString}&symbol={option}&apikey={api_key}"
+    
+    ##this is just so we can check to see if our program will develop the correct url for querying the api
+    print(url)
 
     # Generate Graph and Open in Users Default Browser - In Progress
     if (chartType == 1):
