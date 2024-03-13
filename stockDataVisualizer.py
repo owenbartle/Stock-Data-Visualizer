dcc2975d2c90735204866189ad22a868fed505f9
owenbartle <obbartle01@gmail.com>
2024-03-13 14:16:38 -0500

    Created test branch with some tweaks I made to get started on the graphs

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -7,11 +7,19 @@ import requests
 api_key = 'JLFXYX4J4I20CF8E'
 
 #method for fetching stock date - In Progress
-def fetch_stock_date(symbol, api_key, url):
-    #url
-    #response
-    #data
-    return #data
+def fetch_stock_data(symbol, api_key, tsString):
+    url = f"https://www.alphavantage.co/query?function={tsString}&symbol={symbol}&apikey={api_key}"
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+def fetch_intraday_data(symbol, api_key, tsString, interval):
+    url = f"https://www.alphavantage.co/query?function={tsString}&symbol={symbol}&interval={interval}&apikey={api_key}"
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+    
 
 # Title
 print("Stock Data Visualizer")
@@ -20,13 +28,13 @@ print("-------------------------")
 # Main While Loop
 loop = True
 while (loop == True):
-    # Get Stock Option - Incomplete
-    option = input("Enter the stock symbol to search for: ")
+    # Get Stock Option - Complete
+    symbol = input("Enter the stock symbol to search for: ")
     
     # Select Chart Type - Complete
     print("Chart Types:\n---------------------\n\t1. Bar\n\t2. Line")
     while (True):
-        chartType = input("Enter the type (1, 2)")
+        chartType = input("Enter the type (1, 2): ")
         if (chartType == '1' or chartType == '2'):
             chartType = int(chartType)
             break
@@ -36,14 +44,16 @@ while (loop == True):
     # Select Time Series Type - Complete
     print("Select the Time Series of the chart you want to Generate:")
     print("----------------------------")
-    print("1. Intraday")
-    print("2. Daily")
-    print("3. Weekly")
-    print("4. Monthly\n")
+    print("\t1. Intraday")
+    print("\t2. Daily")
+    print("\t3. Weekly")
+    print("\t4. Monthly\n")
     
     bool = True
     while(bool):
         tsChoice = input("Enter time series option (1, 2, 3, 4): ")
+        if tsChoice == '1':
+            interval = input("Enter time interval (1min, 5min, 15min, 30min, 60min): ")
         try:
             tsChoice = int(tsChoice)
             if(tsChoice == 1 or tsChoice == 2 or tsChoice == 3 or tsChoice == 4):
@@ -57,13 +67,17 @@ while (loop == True):
         
     ##Convert user time series type to api url parameter
     if(tsChoice == 1):
-        tsString = "function=TIME_SERIES_INTRADAY"
+        tsString = "TIME_SERIES_INTRADAY"
+        fetch_intraday_data(symbol, api_key, tsString, interval)
     elif(tsChoice == 2):
-        tsString = "function=TIME_SERIES_DAILY"
+        tsString = "TIME_SERIES_DAILY"
+        fetch_stock_data(symbol, api_key, tsString)
     elif(tsChoice == 3):
-        tsString = "function=TIME_SERIES_WEEKLY"
+        tsString = "TIME_SERIES_WEEKLY"
+        fetch_stock_data(symbol, api_key, tsString)
     else:
-        tsString = "function=TIME_SERIES_MONTHLY"
+        tsString = "TIME_SERIES_MONTHLY"
+        fetch_stock_data(symbol, api_key, tsString)
         
        
         
@@ -90,18 +104,46 @@ while (loop == True):
     
     ##url now queries our api using our key, searching for ouyr given symbol, and using our time series option
     
-    url = f"https://www.alphavantage.co/query?{tsString}&symbol={option}&apikey={api_key}"
-    
     ##this is just so we can check to see if our program will develop the correct url for querying the api
-    print(url)
+    #print(url)
 
     # Generate Graph and Open in Users Default Browser - In Progress
-    if (chartType == 1):
-        barChart = pygal.Bar()
-        barChart.title = 'Bar Chart'
-    elif (chartType == 2):
-        lineChart = pygal.Line()
-        lineChart.title = 'Line Chart'
+    
+    #Data keys
+    #Time Series (interval)
+    #Time Series (Daily)
+    #Weekly Time Series
+    #Monthly Time Series
+    
+    
+    
+    if stockData:
+        if (chartType == 1):
+            barChart = pygal.Bar()
+            barChart.title = f'Bar Char for {symbol} between {startDate} and {endDate}'
+            dates = []
+            openPrices = []
+            highPrices = []
+            lowPrices = []
+            closePrices = []
+            for date, data in stockData[tsString].items():
+                dates.append(date)
+                openPrices.append(float(data['1. open']))
+                highPrices.append(float(data['2. high']))
+                lowPrices.append(float(data['3. low']))
+                closePrices.append(float(data['4. close']))
+                
+            barChart.x_labels = dates
+            barChart.add('Opening Prices', openPrices)
+            barChart.add('High Prices', highPrices)
+            barChart.add('Low Prices', lowPrices)
+            barChart.add('Closing Prices', closePrices)
+            barChart.render_in_browser()
+        elif (chartType == 2):
+            lineChart = pygal.Line()
+            lineChart.title = 'Line Chart'
+        else:
+            print("Chart type invalid.")
 
     # Find another stock - Complete
     while(True):
