7d7c9657c57d8ebaa24849fc54cda0281469598b
owenbartle <obbartle01@gmail.com>
2024-03-13 23:14:03 -0500

    finished the graph rendering as well as made some other tweaks all around

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -23,7 +23,7 @@ def fetch_intraday_data(symbol, api_key, tsString, interval):
 
 # Title
 print("Stock Data Visualizer")
-print("-------------------------")
+print("-------------------------\n")
 
 # Main While Loop
 loop = True
@@ -32,7 +32,7 @@ while (loop == True):
     symbol = input("Enter the stock symbol to search for: ")
     
     # Select Chart Type - Complete
-    print("Chart Types:\n---------------------\n\t1. Bar\n\t2. Line")
+    print("\nChart Types:\n---------------------\n\t1. Bar\n\t2. Line")
     while (True):
         chartType = input("Enter the type (1, 2): ")
         if (chartType == '1' or chartType == '2'):
@@ -49,36 +49,49 @@ while (loop == True):
     print("\t3. Weekly")
     print("\t4. Monthly\n")
     
+    
+    timeSeriesKey = {}
+    
     bool = True
     while(bool):
         tsChoice = input("Enter time series option (1, 2, 3, 4): ")
         if tsChoice == '1':
-            interval = input("Enter time interval (1min, 5min, 15min, 30min, 60min): ")
-        try:
-            tsChoice = int(tsChoice)
-            if(tsChoice == 1 or tsChoice == 2 or tsChoice == 3 or tsChoice == 4):
-                bool = False
+            interval = input("Enter time interval (1. 1min, 2. 5min, 3. 15min, 4. 30min, 5. 60min): ")
+            if (interval == '1'):
+                interval = "1min"
+            elif (interval == '2'):
+                interval = "5min"
+            elif (interval == '3'):
+                interval = "15min"
+            elif (interval == '4'):
+                interval = "30min"
+            elif (interval == '5'):
+                interval = "60min"
             else:
-                raise Exception("Not a valid choice. Enter only a '1', '2', '3', or '4', Exception")
-        except:
-            print("Not a valid choice. Enter only a '1', '2', '3', or '4'.", Exception)
-    
-    tsString = ' '
-        
-    ##Convert user time series type to api url parameter
-    if(tsChoice == 1):
-        tsString = "TIME_SERIES_INTRADAY"
-        fetch_intraday_data(symbol, api_key, tsString, interval)
-    elif(tsChoice == 2):
-        tsString = "TIME_SERIES_DAILY"
-        fetch_stock_data(symbol, api_key, tsString)
-    elif(tsChoice == 3):
-        tsString = "TIME_SERIES_WEEKLY"
-        fetch_stock_data(symbol, api_key, tsString)
-    else:
-        tsString = "TIME_SERIES_MONTHLY"
-        fetch_stock_data(symbol, api_key, tsString)
-        
+                print("Invalid input. Must enter a number 1-5.")
+                continue
+            tsString = "TIME_SERIES_INTRADAY"
+            stockData = fetch_intraday_data(symbol, api_key, tsString, interval)
+            timeSeriesKey = {1: f"Time Series {interval}"}
+            break        
+        elif(tsChoice == '2'):
+            tsString = "TIME_SERIES_DAILY"
+            stockData = fetch_stock_data(symbol, api_key, tsString)
+            timeSeriesKey = {2: "Time Series (Daily)"}
+            break
+        elif(tsChoice == '3'):
+            tsString = "TIME_SERIES_WEEKLY"
+            stockData = fetch_stock_data(symbol, api_key, tsString)
+            timeSeriesKey = {3: "Weekly Time Series"}
+            break
+        elif (tsChoice == '4'):
+            tsString = "TIME_SERIES_MONTHLY"
+            stockData = fetch_stock_data(symbol, api_key, tsString)
+            timeSeriesKey = {4: "Monthly Time Series"}
+            break
+        else:
+            print("Invalid choice. Must enter 1. (Intraday), 2. (Daily) 3. (Weekly) 4. (Monthly)")
+            continue
        
         
     # Select Start Date - Complete
@@ -100,33 +113,30 @@ while (loop == True):
     
     ##change user choices to api parameters
     ##variables tsString for time series option,
-    ##symbol={option}
     
     ##url now queries our api using our key, searching for ouyr given symbol, and using our time series option
     
     ##this is just so we can check to see if our program will develop the correct url for querying the api
-    #print(url)
-
-    # Generate Graph and Open in Users Default Browser - In Progress
     
-    #Data keys
+    # Generate Graph and Open in Users Default Browser - COMPLETE (thank the lord)
+    
+    #Data keys using a python dictionary to define
     #Time Series (interval)
     #Time Series (Daily)
     #Weekly Time Series
     #Monthly Time Series
     
-    
-    
     if stockData:
         if (chartType == 1):
             barChart = pygal.Bar()
             barChart.title = f'Bar Char for {symbol} between {startDate} and {endDate}'
+            
             dates = []
             openPrices = []
             highPrices = []
             lowPrices = []
             closePrices = []
-            for date, data in stockData[tsString].items():
+            for date, data in stockData[timeSeriesKey[int(tsChoice)]].items():
                 dates.append(date)
                 openPrices.append(float(data['1. open']))
                 highPrices.append(float(data['2. high']))
@@ -139,15 +149,36 @@ while (loop == True):
             barChart.add('Low Prices', lowPrices)
             barChart.add('Closing Prices', closePrices)
             barChart.render_in_browser()
+            
         elif (chartType == 2):
             lineChart = pygal.Line()
-            lineChart.title = 'Line Chart'
+            lineChart.title = f'Line Chart for {symbol} between {startDate} and {endDate}'
+            
+            
+            dates = []
+            openPrices = []
+            highPrices = []
+            lowPrices = []
+            closePrices = []
+            for date, data in stockData[timeSeriesKey[int(tsChoice)]].items():
+                dates.append(date)
+                openPrices.append(float(data['1. open']))
+                highPrices.append(float(data['2. high']))
+                lowPrices.append(float(data['3. low']))
+                closePrices.append(float(data['4. close']))
+                
+            lineChart.x_labels = dates
+            lineChart.add('Opening Prices', openPrices)
+            lineChart.add('High Prices', highPrices)
+            lineChart.add('Low Prices', lowPrices)
+            lineChart.add('Closing Prices', closePrices)
+            lineChart.render_in_browser()
         else:
             print("Chart type invalid.")
 
     # Find another stock - Complete
     while(True):
-        visualizeAnother = input("Would you like to view more stock data? Press 'y' to continue : ")
+        visualizeAnother = input("Would you like to view more stock data? Press 'y' to continue any key to exit: ")
         if (visualizeAnother == 'y'):
             break
         else:
