a4fe7c331fc24dc9ce620e8b54896ec06761757c
Connor V <11cvantress@gmail.com>
2024-04-09 18:24:18 -0500

    cleaning

--- flaskApp.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
-
-app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-    return render_template('index.html')
